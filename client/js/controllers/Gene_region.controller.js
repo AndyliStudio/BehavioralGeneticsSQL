0251496ab8013d301dc7033ab0e49a94aea928be
lidikang <lidikang@myhexin.com>
2017-07-12 20:46:54 +0800

    开始angular页面的编写

---
+++ client/js/controllers/Gene_region.controller.js
@@ -0,0 +1,16 @@
+angular
+  .module('app')
+  .controller('Gene_regionController', ['$scope', 'AuthService', '$state',
+      function($scope, AuthService, $state) {
+    $scope.user = {
+      email: 'foo@bar.com',
+      password: 'foobar'
+    };
+
+    $scope.login = function() {
+      AuthService.login($scope.user.email, $scope.user.password)
+        .then(function() {
+          $state.go('add-review');
+        });
+    };
+  }]);
