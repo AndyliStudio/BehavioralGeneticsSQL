683509945889de1ef5c65511472ec47249461921
andyliwr <andyliwr@outlook.com>
2017-08-29 23:13:25 +0800

    通过分析mysql日志找到查询数据不对的原因了, gte和lte不能共用

--- client/js/controllers/auth.js
+++ client/js/controllers/auth.js
@@ -379,8 +379,7 @@ angular
                   filter: {
                     where: {
                       pos: {
-                        gte: res[0].start,
-                        ite: res[0].stop
+                        between: [parseInt(res[0].start), parseInt(res[0].stop)]
                       }
                     },
                     fields: {
@@ -436,19 +435,35 @@ angular
       };
     }
   ])
-  .controller('Gene_regionController', ['$scope', 'Snp_info', 'Gene_info', 'Pathway', '$state',
-    function ($scope, Snp_info, Gene_info, Pathway, $state) {
+  .controller('Gene_regionController', ['$scope', 'Snp_info', 'Gene_info', 'Pathway', '$state', '$http',
+    function ($scope, Snp_info, Gene_info, Pathway, $state, $http) {
       $scope.sstr = ''
       $scope.suggests = []
       $scope.thisPathway = {}
       $scope.isShowTable = false
       $scope.isShowSuggest = false // wheather show suggest panel
       $scope.showLoading = false // wheather show loading panel
-      $scope.chartData = []
-      $scope.timer = null
+      $scope.allSnps = []
+      $scope.showingSnps = []
+      $scope.currentPage = 0
+      $scope.totalItems = 0
+      $scope.isSelectAll = false
+      $scope.selectIds = []
+      $scope.download_fail_text = ''
+      $scope.convertFloat = function (str) {
+        let regExp = /^\d+(\.)*\d+[Ee]{1}(-)*\d+$/ig
+        if (regExp.test(str)) {
+          let num = parseFloat(str.substring(0, str.indexOf('e') > -1 ? str.indexOf('e') : (str.indexOf('E') > -1 ? str.indexOf('E') : 0)))
+          let exc = parseInt(str.substring((str.indexOf('e') > -1 ? str.indexOf('e') : (str.indexOf('E') > -1 ? str.indexOf('E') : 0)) + 1))
+          return num * Math.pow(10, exc)
+        } else {
+          return parseFloat(str)
+        }
+      }
       $scope.searchGeneRegion = function () {
         if($scope.sstr){
           $scope.isShowSuggest = true
+          $scope.showLoading = true
           Gene_info.find({
             filter: {
               where: {
@@ -459,7 +474,11 @@ angular
               fields: {
                 start: true,
                 stop: true,
+                gene_id: true,
                 id: true
+              },
+              sort: {
+                gene_id: -1
               }
             }
           })
@@ -470,103 +489,111 @@ angular
                   if ($scope.suggests.length < 10) {
                     $scope.suggests.push({
                       id: item.id,
+                      gene_id: item.gene_id,
                       start: item.start,
                       stop: item.stop
                     })
                   }
                 })
               }
+              $scope.showLoading = false
             })
         }else{
           $scope.isShowSuggest = false
+          $scope.showLoading = false
         }
       };
-      $scope.showGeneRegion = function (text, id, genes) {
+      $scope.showGeneRegion = function (gene_id, start, stop) {
         var self = $scope
-        $scope.sstr = text
-        $scope.chartData = []
+        $scope.sstr = gene_id
+        
         // close search suggest
         $scope.isShowSuggest = false
         $scope.showLoading = true
         $scope.isShowTable = false
-        // 遍历所有的genes
-        genes.forEach((item, index) => {
-          $scope.chartData.push({sign: '', result: [], name: item, range: '', length: 0})
-          Gene_info.find({
-            filter: {
-              where: {
-                gene_id: item
-              },
-              fields: {
-                start: true,
-                stop: true
-              },
-              limit: 1
+        // search snp_info
+        Snp_info.find({
+          filter: {
+            where: {
+              pos: {  
+                between: [parseInt(start), parseInt(stop)]
+              }
+            },
+            fields: {
+              id: true,
+              trait: true,
+              marker: true,
+              p: true
+            }
+          }
+        })
+          .$promise
+          .then(function (snpRes){
+            if(snpRes){
+              $scope.isShowSuggest = false
+              $scope.showLoading = false
+              $scope.isShowTable = true
+              $scope.totalItems = snpRes.length || 0
+              $scope.showingSnps = snpRes.splice(0, 10).map(item => {
+                item.isChecked = false
+                return item;
+              })
+              $scope.allSnps = snpRes.map(item => {
+                item.isChecked = false
+                return item;
+              })
             }
           })
-            .$promise
-            .then(function (res) {
-              if(res instanceof Array && res.length > 0){
-                // find all snps where pos bettwen start_pos and end_pos
-                self.chartData[index].range = '['+res[0].start+','+res[0].stop+']'
-                Snp_info.find({
-                  filter: {
-                    where: {
-                      pos: {
-                        gte: res[0].start,
-                        ite: res[0].stop
-                      }
-                    },
-                    fields: {
-                      id: true,
-                      pos: true
-                    }
-                  }
-                })
-                  .$promise
-                  .then(function (snpRes){
-                    if(snpRes){
-                      self.chartData[index].sign = 'success'
-                      self.chartData[index].result = snpRes
-                      self.chartData[index].length = snpRes.length
-                    }else{
-                      self.chartData[index].sign = 'fail'
-                    }
-                  })
-              }else{
-                self.chartData[index].sign = 'fail'
-                console.log("can't found such gene_info where gene_id is " + item)
-              }
-            })
+      };
+      $scope.pageChanged = function () {
+        $scope.showingSnps = $scope.allSnps.splice($scope.currentPage * 10, 10)
+      }
+      $scope.itemSelected = function (select, id) {
+        let isExist = $scope.selectIds.some(item => {
+          return item === id
         })
-        self.timer = setInterval(function(){
-          var that = $scope
-          var isAllReady = $scope.chartData.every(item => {
-            return item.sign === 'success' || item.sign === 'fail'
-          })
-          var str = ''
-          $scope.chartData.forEach(item => {
-            str += '('+item.sign + ') '
-          })
-          console.log(str)
-          if(isAllReady){
-            that.isShowSuggest = false
-            that.showLoading = false
-            that.isShowTable = true
-            that.$apply();//需要手动刷新 
-            clearInterval(that.timer)
+        if (select) {
+          if (!isExist) {
+            $scope.selectIds.push(id)
           }
-        }, 100)
-      };
-      var count = 0;
-      $scope.displayFunction = function () {
-          count++;
-          return count+ ' ' +this.name;
-      };
-
-      $scope.tootipFunction = function () {
-          var color = "style=\"color: "+this.series.color+"\"";
-          return  "<span "+color+">基因名字："+ this.x +"</span><br>"+"<span "+color+">"+"包含的显著snp个数："+": </span> <b>"+this.point.y+"</b><br><span>显著snp位置区间：<b>"+this.point.range+"</b></span>"
-      };
+        } else {
+          if (!isExist) {
+            $scope.selectIds.splice($scope.selectIds.indexOf(id), 1)
+          }
+        }
+      }
+      $scope.selectAll = function () {
+        $scope.selectIds = []
+        if ($scope.isSelectAll) {
+          $scope.allSnps.forEach(item => {
+            item.isChecked = true
+            $scope.selectIds.push(item.id)
+          })
+        } else {
+          $scope.allSnps.forEach(item => {
+            item.isChecked = false
+          })
+        }
+        $scope.showingSnps = $scope.allSnps.splice($scope.currentPage * 10, 10)
+      }
+      $scope.download = function () {
+        // 获取所有选中的snpinfo的id
+        $http({
+          method: 'post',
+          url: '/api/snp_infos/downloadSnp',
+          data: {
+            ids: $scope.selectIds
+          }
+        }).then(function successCallback(response) {
+          if (response.data.result.errno === 0) {
+            alert(response.data.result.download_url)
+            window.open(response.data.result.download_url)
+          } else {
+            $scope.download_fail_text = '下载失败，' + response.data.result.message
+          }
+        }, function errorCallback(error) {
+          $scope.download_fail_text = '下载失败，' + error.toString()
+        });
+      }
     }
   ]);
